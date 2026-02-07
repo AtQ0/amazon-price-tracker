978ae9d780c7a7046bbd840edfffc97b1655f222
AtQ0 <mail@atkobabic.com>
2026-02-07 15:07:31 +0100

    added header with icon avatar and search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { auth } from "@/auth";
 import LoginView from "./LoginView";
+import Header from "@/components/Header";
 
 export default async function Home() {
   const session = await auth();
   const user = session?.user;
   return (
     <div>
-      {user && <div>Hello {user.name}</div>}
+      {user && (
+        <div className="p-4">
+          <Header user={user} />
+        </div>
+      )}
       {!user && <LoginView />}
     </div>
   );
